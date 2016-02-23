e79e00bf4691d163df041ff3299165c8d7fc5d46
mikkobasilio <mikkobasilio@gmail.com>
2016-02-23 15:49:47 +0800

    Added files via upload

--- EnrollmentSystem.java
+++ EnrollmentSystem.java
@@ -1,6 +1,8 @@
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,56 +15,188 @@ import java.util.Iterator;
  * @author Student
  */
 public class EnrollmentSystem {
-    
-    ArrayList<Course> courses;
-    
-    public EnrollmentSystem()
-    {
-        courses = new ArrayList<>();
-    }
-    
-    public void register(Student student){}
-    
-    public boolean checkLogin(String id, String password)
-    {
-        //placeholder
-        return true;
-    }
-    
-    public void openLogin(boolean open)
-    {
+    DBUtil db;
+    public EnrollmentSytem(){
+        db = new DBUtil();
         
     }
     
-    public void openRegister(boolean open)
-    {
-        
-    }
-    
-    public void openViewCourses(boolean open)
-    {
-        
-    }
-    
-    public Iterator<String> getCourses()
-    {
-        ArrayList<String> courseCodes = new ArrayList<>();
-        
-        for (Course course : courses) {
-            courseCodes.add(course.getCouseCode());
-        }
-        
-        return courseCodes.iterator();
-    }
-    
-    public void enrollCourse(String idNum, String courseCode)
-    {
-        System.out.println("ALLAHU AKBAR");
-    }
-    
-    public boolean canEnroll()
-    {
-        return true;
-    }
+    public ArrayList<Course> viewAllCourses(){
+        ArrayList<Course> courses = new ArrayList<Course>();
+        try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * from course");
             
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Course tempCourse = new Course(rs.getString("code"), rs.getInt("units"), rs.getInt("max"));
+                courses.add(tempCourse);  
+            }
+        }
+        catch(Exception se){       	}
+        
+        return courses;
+    }
+    
+    public ArrayList<Course> viewAllEnrolledCourses(String idNum){
+        ArrayList<Course> courses = new ArrayList<Course>();
+        
+        try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * from course, student_course WHERE course.code = student_course.code AND student_course.id = '" + idNum + "'");
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Course tempCourse = new Course(rs.getString("code"), rs.getInt("units"), rs.getInt("max"));
+                courses.add(tempCourse);  
+            }
+        }
+        catch(Exception se){       	}
+        return courses;
+    }
+    
+    public boolean checkLogin(String idNum, String password){
+        
+        try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * from student WHERE idNum = '" + idNum + "' AND password = '" + password +"'");
+            
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                return true;
+            }
+        }catch(Exception se){}
+        return false;
+    }
+    
+    public boolean checkID(String idNum){
+        try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT id from student");
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                if(idNum.equals(rs.getString("id"))){
+                    return false;
+                }
+            }
+        }catch(Exception se){}
+        return true;
+    }
+    
+    public boolean register(Student student, String password){
+        String name, idNum;
+        name = student.getName();
+        idNum = student.getID();
+        if(checkID(idNum)){
+            try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("INSERT INTO student (`fullname`, `id`, `password`) VALUES(?,?,?)");
+            ps.setString(1, name);
+            ps.setString(2, idNum);
+            ps.setString(3, password);
+            
+            int rs = ps.executeUpdate();
+            if(rs>0){
+                return true;
+            }
+        }catch(Exception se){}
+        }
+        return false;
+    }
+    
+    public boolean enrollCourse(String idNum, String courseCode){
+        if(canEnroll(idNum, courseCode)){
+            try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("INSERT INTO student_course (`id`, `code`) VALUES(?,?)");
+            ps.setString(1, idNum);
+            ps.setString(2, courseCode);
+            
+            int rs = ps.executeUpdate();
+            if(rs>0){
+                return true;
+            }
+        }catch(Exception se){}
+        }
+        return false;
+    }
+    
+    public boolean canEnroll(String idNum, String courseCode){
+        if(checkCourseCode(courseCode)){
+            if(checkStudentMax(idNum) && checkCourseMax(courseCode)){
+                return true;
+            } else return false;
+        }else return false;
+    }
+    
+    private boolean checkCourseCode(String courseCode){
+        try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * from course WHERE code = '" + courseCode +"'");
+            
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                return true;
+            }
+        }catch(Exception se){}
+        return false;
+    }
+    
+    private boolean checkCourseMax(String courseCode){
+        int max = 0, curr = 0;
+        try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT max from course WHERE code = '" + courseCode +"'");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            max = rs.getInt("max");
+        }catch(Exception se){}
+        
+        try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT count(*) AS benjobobo from student_course WHERE code = '" + courseCode +"'");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            curr  = rs.getInt("benjobobo");
+        }catch(Exception se){}
+        
+        if(curr < max){
+            return true;
+        }else return false;
+    }
+    
+    private boolean checkStudentMax(String idNum){
+        int max = 0, curr = 0;
+         try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT count(*) AS benjobobo from student_course WHERE id = '" + idNum +"'");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            curr  = rs.getInt("benjobobo");
+        }catch(Exception se){}
+         
+        if(curr < 9){
+            return true;
+        }else return false;
+    }
+   
+    
+    public boolean dropCourse(String idNum, String courseCode){
+        if(checkCourseCode(courseCode)){
+            try{
+            Connection  con= db.getConnection();
+            PreparedStatement ps = con.prepareStatement("DELETE from student_course WHERE id = '" + idNum +"' AND code='" + courseCode +"'");
+            int rs = ps.executeUpdate();
+            if(rs>0){
+                return true;
+            }
+            }catch(Exception se){}
+        }
+        return false;
+    }
+    
+    
 }
